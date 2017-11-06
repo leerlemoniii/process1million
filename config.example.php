0f46b013e7bb9b0558eebf0c3ed83b47cf501911
Lee Lemon <leerlemoniii@gmail.com>
2017-11-05 23:39:14 -0500

    updated comment in config example

--- config.example.php
+++ config.example.php
@@ -1,5 +1,5 @@
 <?php
-// modify this file for your sql server
+// modify this file for your sql server then save to config.php
 $myserver = "localhost"; //your server address
 $myuser = ""; //your user
 $mypassword = "";//your password
