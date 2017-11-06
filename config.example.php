708a6231fcd8edf7d50e05add00a75ed9ab17fa2
Lee Lemon <leerlemoniii@gmail.com>
2017-11-05 23:37:18 -0500

    added example config file

---
+++ config.example.php
@@ -0,0 +1,7 @@
+<?php
+// modify this file for your sql server
+$myserver = "localhost"; //your server address
+$myuser = ""; //your user
+$mypassword = "";//your password
+$mydb = "millionrecord";
+$mytable = "user";
